ce8f3478d84f7b155e5458b219042127f6c7526f
Saber <luissaez261002@gmail.com>
2023-06-14 20:12:16 -0400

    Se agrego listado de ingredientes

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.db.models import PROTECT, CASCADE
-
+import uuid
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 # tablas necesarias para el retiro en tienda
 class Ingrediente(models.Model):
-    id_ing = models.TextField(primary_key=True, max_length=50)
+    id_ing = models.UUIDField(primary_key=True, default=uuid.uuid4())
     nombre_ing = models.TextField(max_length=15, unique=True, blank=False, null=False)
     unidad_medida = models.TextField(null=False, blank=False, max_length=15)
     
@@ -13,8 +14,15 @@ class Ingrediente(models.Model):
         return str(f'{self.nombre_ing}')
     
 class Plato(models.Model):
-    id_plato = models.BigAutoField(primary_key=True)
+    id_plato = models.UUIDField(primary_key=True, default=uuid.uuid4())
     nombre_plato = models.TextField(unique=True, max_length=15, blank=False, null=False)
     
     def __str__(self):
         return str(f'{self.nombre_plato}')
+
+class DetallePlato(models.Model):
+    id_dPlato = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    id_plato = models.ForeignKey(Plato, on_delete=PROTECT)
+    id_ingrediente = models.ForeignKey(Ingrediente, on_delete=PROTECT)
+    cantidad = models.IntegerField(null=False, default=0, validators=(MinValueValidator(1), MaxValueValidator(9999)))
+    
